5416ac71ae710d10518122dedbc5dd81f3d8c729
Jeongsoo Park <toracle@gmail.com>
2015-10-01 22:25:59 +0900

    initial commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject clj-korea-real-estate "1.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
---
+++ src/main.clj
@@ -0,0 +1,5 @@
+(ns clj-korea-real-estate)
+
+(def rtms-mobile "http://rtmobile.molit.go.kr/app/main.jsp")
+(def rtms "http://rt.molit.go.kr/rtApt.do?cmd=srhLocalView")
+
